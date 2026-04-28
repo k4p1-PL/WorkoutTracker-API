85152aedca10539d6e23d5fc6ba88fa7dc69c43f
k4p1-PL <sokol.kacper23@gmail.com>
2026-04-28 21:53:34 +0200

    Update

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
 });
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -17,7 +22,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddScoped<WorkoutTracker.Interfaces.IDashboardService, WorkoutTracker.Services.DashboardService>();
 var app = builder.Build();
-
+app.UseCors("AllowReactApp");
 // Odpalamy Swaggera
 if (app.Environment.IsDevelopment())
 {
